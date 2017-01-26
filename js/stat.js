82a1a102ede3750b4ac08f6dd5751ee6646932c1
firefoxic <firefoxic.arts@gmail.com>
2017-01-26 18:25:53 +0300

    Нарисуй тень под облаком

--- js/stat.js
+++ js/stat.js
@@ -2,6 +2,9 @@
 
 window.renderStatistics = function (ctx, names, times) {
 
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.fillRect(110, 20, 420, 270);
+
   ctx.fillStyle = 'rgba(256, 256, 256, 1.0)';
   ctx.strokeRect(100, 10, 420, 270);
   ctx.fillRect(100, 10, 420, 270);
