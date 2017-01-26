054c506d38a2c286a977903ee8037baba6e5d71f
firefoxic <firefoxic.arts@gmail.com>
2017-01-26 18:19:26 +0300

    Нарисуй белое прямоугольное облако

--- js/stat.js
+++ js/stat.js
@@ -1,1 +1,9 @@
 'use strict';
+
+window.renderStatistics = function (ctx, names, times) {
+
+  ctx.fillStyle = 'rgba(256, 256, 256, 1.0)';
+  ctx.strokeRect(100, 10, 420, 270);
+  ctx.fillRect(100, 10, 420, 270);
+
+};
