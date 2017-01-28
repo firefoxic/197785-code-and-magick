42077d0a71c6a08f8db3e75df6ebf8ebe6ca9b0d
firefoxic <firefoxic.arts@gmail.com>
2017-01-28 10:14:57 +0300

    Добавь закрытие окна настройки персонажа
    Окно должно закрываться по нажатию на элемент .setup-close,
    расположенный внутри окна.

--- js/setup.js
+++ js/setup.js
@@ -2,7 +2,12 @@
 
 var setup = document.querySelector('.setup');
 var setupOpen = document.querySelector('.setup-open');
+var setupClose = setup.querySelector('.setup-close');
 
 setupOpen.addEventListener('click', function () {
   setup.classList.remove('invisible');
+});
+
+setupClose.addEventListener('click', function () {
+  setup.classList.add('invisible');
 });
