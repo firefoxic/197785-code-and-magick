33cf3883e8fd89d6507863cbfe8917baa59bf522
firefoxic <firefoxic.arts@gmail.com>
2017-02-05 15:31:00 +0300

    Упрости код смены цветов
    Элементы, меняющие цвет, и списки возможных цветов, соответствующих
    этим элементам — связанные вещи, поэтму их лучше объединить в объекты.
    Это позволяет при создании обработчиков событий пользоваться общей
    функцией, принимающей в качестве аргумента сами объекты с элементами
    и цветами.

--- js/setup.js
+++ js/setup.js
@@ -3,31 +3,55 @@
 var setup = document.querySelector('.setup');
 var setupOpen = document.querySelector('.setup-open');
 var setupClose = setup.querySelector('.setup-close');
-var wizardCoat = document.querySelector('#wizard-coat');
-var wizardEyes = document.querySelector('#wizard-eyes');
-var fireball = document.querySelector('.setup-fireball-wrap');
-var wizardCoatColors = [
-  'rgb(101, 137, 164)',
-  'rgb(241, 43, 107)',
-  'rgb(146, 100, 161)',
-  'rgb(56, 159, 117)',
-  'rgb(215, 210, 55)',
-  'rgb(0, 0, 0)'
-];
-var wizardEyesColors = [
-  'rgb(0, 0, 0)',
-  'rgb(255, 0, 0)',
-  'rgb(0, 0, 255)',
-  'rgb(255, 255, 0)',
-  'rgb(0, 128, 0)'
-];
-var fireballColors = [
-  'rgb(238, 72, 48)',
-  'rgb(48, 168, 238)',
-  'rgb(92, 230, 192)',
-  'rgb(232, 72, 213)',
-  'rgb(230, 232, 72)'
-];
+
+var wizardCoat = {
+  element: setup.querySelector('#wizard-coat'),
+  colors: [
+    'rgb(101, 137, 164)',
+    'rgb(241, 43, 107)',
+    'rgb(146, 100, 161)',
+    'rgb(56, 159, 117)',
+    'rgb(215, 210, 55)',
+    'rgb(0, 0, 0)'
+  ]
+};
+
+var wizardEyes = {
+  element: setup.querySelector('#wizard-eyes'),
+  colors: [
+    'rgb(0, 0, 0)',
+    'rgb(255, 0, 0)',
+    'rgb(0, 0, 255)',
+    'rgb(255, 255, 0)',
+    'rgb(0, 128, 0)'
+  ]
+};
+
+var fireball = {
+  element: setup.querySelector('.setup-fireball'),
+  colors: [
+    'rgb(238, 72, 48)',
+    'rgb(48, 168, 238)',
+    'rgb(92, 230, 192)',
+    'rgb(232, 72, 213)',
+    'rgb(230, 232, 72)'
+  ]
+};
+
+var getRandomElement = function (array) {
+  var randomElementIndex = Math.floor(Math.random() * array.length);
+  return array[randomElementIndex];
+};
+
+var changeColor = function (item) {
+  item.element.addEventListener('click', function (event) {
+    item.element.setAttribute('fill', getRandomElement(item.colors));
+  });
+};
+
+changeColor(wizardCoat);
+changeColor(wizardEyes);
+changeColor(fireball);
 
 setupOpen.addEventListener('click', function () {
   setup.classList.remove('invisible');
@@ -35,19 +59,4 @@ setupOpen.addEventListener('click', function () {
 
 setupClose.addEventListener('click', function () {
   setup.classList.add('invisible');
-});
-
-wizardCoat.addEventListener('click', function () {
-  var colorNumber = Math.floor(Math.random() * wizardCoatColors.length);
-  wizardCoat.style.fill = wizardCoatColors[colorNumber];
-});
-
-wizardEyes.addEventListener('click', function () {
-  var colorNumber = Math.floor(Math.random() * wizardEyesColors.length);
-  wizardEyes.style.fill = wizardEyesColors[colorNumber];
-});
-
-fireball.addEventListener('click', function () {
-  var colorNumber = Math.floor(Math.random() * fireballColors.length);
-  fireball.style.backgroundColor = fireballColors[colorNumber];
 });
