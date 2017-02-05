814181ef4cce256ec452af8c1e8368b7608614ee
firefoxic <firefoxic.arts@gmail.com>
2017-02-05 10:07:37 +0300

    Реогранизуй предыдущий скрипт
    Для дальнейших изменений нужен порядок.

--- js/setup.js
+++ js/setup.js
@@ -3,6 +3,31 @@
 var setup = document.querySelector('.setup');
 var setupOpen = document.querySelector('.setup-open');
 var setupClose = setup.querySelector('.setup-close');
+var wizardCoat = document.querySelector('#wizard-coat');
+var wizardEyes = document.querySelector('#wizard-eyes');
+var fireball = document.querySelector('.setup-fireball-wrap');
+var wizardCoatColors = [
+  'rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)'
+];
+var wizardEyesColors = [
+  'rgb(0, 0, 0)',
+  'rgb(255, 0, 0)',
+  'rgb(0, 0, 255)',
+  'rgb(255, 255, 0)',
+  'rgb(0, 128, 0)'
+];
+var fireballColors = [
+  'rgb(238, 72, 48)',
+  'rgb(48, 168, 238)',
+  'rgb(92, 230, 192)',
+  'rgb(232, 72, 213)',
+  'rgb(230, 232, 72)'
+];
 
 setupOpen.addEventListener('click', function () {
   setup.classList.remove('invisible');
@@ -12,44 +37,15 @@ setupClose.addEventListener('click', function () {
   setup.classList.add('invisible');
 });
 
-var wizard = document.querySelector('#wizard');
-var wizardCoat = wizard.querySelector('#wizard-coat');
-var wizardCoatColors = [
-  'rgb(101, 137, 164)',
-  'rgb(241, 43, 107)',
-  'rgb(146, 100, 161)',
-  'rgb(56, 159, 117)',
-  'rgb(215, 210, 55)',
-  'rgb(0, 0, 0)'
-];
-
-wizard.addEventListener('click', function () {
+wizardCoat.addEventListener('click', function () {
   var colorNumber = Math.floor(Math.random() * wizardCoatColors.length);
   wizardCoat.style.fill = wizardCoatColors[colorNumber];
 });
-
-var wizardEyes = wizard.querySelector('#wizard-eyes');
-var wizardEyesColors = [
-  'black',
-  'red',
-  'blue',
-  'yellow',
-  'green'
-];
 
 wizardEyes.addEventListener('click', function () {
   var colorNumber = Math.floor(Math.random() * wizardEyesColors.length);
   wizardEyes.style.fill = wizardEyesColors[colorNumber];
 });
-
-var fireball = document.querySelector('.setup-fireball-wrap');
-var fireballColors = [
-  '#ee4830',
-  '#30a8ee',
-  '#5ce6c0',
-  '#e848d5',
-  '#e6e848'
-];
 
 fireball.addEventListener('click', function () {
   var colorNumber = Math.floor(Math.random() * fireballColors.length);
